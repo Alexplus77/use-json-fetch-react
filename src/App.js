55c7002ef306e6985bc7dcf30a9af82e0aaf9f89
alexs <alexplus77@mail.ru>
2021-12-18 13:19:14 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ function App() {
   const urlData = "http://localhost:7070/data";
   const urlError = "http://localhost:7070/error";
   const urlLoading = "http://localhost:7070/loading";
+  const opts = { method: "GET" };
 
-  const [{ data, error, loading }] = useJsonFetch(urlData);
-  console.log(data, error, loading);
-  return <div className="App"></div>;
+  const [{ data, error, loading, responseFetch }] = useJsonFetch(
+    urlLoading,
+    opts,
+    4000
+  );
+  console.log(data, error, loading, responseFetch());
+  return <div className="App">{responseFetch()}</div>;
 }
 
 export default App;
--- src/hooks/useJsonFetch.js
+++ src/hooks/useJsonFetch.js
@@ -1,17 +1,42 @@
 import { useEffect, useState } from "react";
 
-const useJsonFetch = (url, opts) => {
+const useJsonFetch = (url, opts, timeout) => {
   const [error, setError] = useState(false);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [responseTimeout, setResponseTimout] = useState(null);
+
   useEffect(() => {
+    !timeout && (timeout = 10000);
+
     fetch(url, opts)
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((err) => setError(err))
       .finally(() => setLoading(false));
+
+    const idTimer = setTimeout(() => {
+      loading && setResponseTimout("Превышено время ожидания");
+    }, timeout);
+    return () => clearTimeout(idTimer);
   }, []);
-  return [{ data, error, loading }];
+
+  const responseFetch = () => {
+    if (responseTimeout) {
+      return "Превышено время ожидания";
+    }
+    if (data) {
+      return `data status: ${data.status}`;
+    }
+    if (error) {
+      return "Произошла ошибка";
+    }
+    if (loading && !responseTimeout) {
+      return "Loading...";
+    }
+  };
+
+  return [{ data, error, loading, responseFetch }];
 };
 
 export { useJsonFetch };
